3afaa5137926014eadfbe71e7127c591f9e9a376
sobee-dev <obinnasamueledeh10000@gmail.com>
2026-04-03 03:44:58 +0100

    fix cors and supabase connection

--- receipt_backend_api/settings.py
+++ receipt_backend_api/settings.py
@@ -34,6 +34,7 @@ if DATABASE_URL:
         "default": dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
+            ssl_require=True,
         )
     }
 else:
